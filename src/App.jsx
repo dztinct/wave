83e3054bf751f3585b52a68e479a7aa892e41fde
dztinct <detunjinov@gmail.com>
2024-08-22 12:03:27 +0100

    Error page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Services from './pages/Services'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Error from './pages/Error'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -17,6 +18,7 @@ const App = () => {
           <Route path='/services' element={<Services/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/contact' element={<Contact/>}/>
+          <Route path='*' element={<Error/>}/>
         </Routes>
         <Footer/>
       </Router>
